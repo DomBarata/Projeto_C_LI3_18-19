9753d3588e4774e9a9682fcdcee98863e542b756
Tiago Barata <49235036+DomBarata@users.noreply.github.com>
2019-04-05 15:17:56 +0100

    Delete SGV.h

--- SGV.h
+++
@@ -1,8 +0,0 @@
-#include "CatProdutos"
-#include "CatClientes"
-#include "Facturacao"
-#include "Filial"
-
-typedef struct sgv* SGV;
-
-SGV CriaSGV(CatProdutos catp, CatClientes catc, Facturacao f, Filial fil1, Filial fil2, Filial fil3);
